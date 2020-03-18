9899e8b92a6c428884dbc1b69e8ba82a59116210
Mohsen Einhesari <mohseneinhesari@yahoo.com>
2020-03-18 15:25:02 +0330

    add Consts

---
+++ app/src/main/java/com/einhesari/zomatosample/utils/Const.kt
@@ -0,0 +1,9 @@
+package com.einhesari.zomatosample.utils
+
+object Const {
+    const val REQUEST_TIMEOUT = 60 // 60 Seconds
+    const val ZOMATO_BASE_URL = "https://developers.zomato.com/api/v2.1/"
+    const val CACHE_MAX_AGE = 5 // 5 Seconds
+    const val CACHE_MAX_STALE = 60 * 60 * 24 // 1 Day
+
+}
