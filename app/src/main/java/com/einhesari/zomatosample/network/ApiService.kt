cf16c47d8494bb6014ef3dd5d4d2d56af0d098f4
Mohsen Einhesari <mohseneinhesari@yahoo.com>
2020-03-21 17:05:54 +0430

    fix queries in api service

--- app/src/main/java/com/einhesari/zomatosample/network/ApiService.kt
+++ app/src/main/java/com/einhesari/zomatosample/network/ApiService.kt
@@ -3,9 +3,15 @@ package com.einhesari.zomatosample.network
 import com.einhesari.zomatosample.model.RestaurantSearchResponse
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
+
     @GET("/search")
-    fun findRestaurant(lat: String, long: String, radius: String): Single<RestaurantSearchResponse>
+    fun findRestaurant(
+        @Query("lat") lat: String,
+        @Query("lon") long: String,
+        @Query("radius") radius: String
+    ): Single<RestaurantSearchResponse>
 
 }
